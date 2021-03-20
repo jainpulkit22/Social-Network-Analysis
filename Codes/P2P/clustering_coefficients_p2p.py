f0a4531840ec7a06be0e6a54ed25809de08e4126
Tushar Singhal <42600407+codesurf@users.noreply.github.com>
2021-03-20 16:06:19 +0530

    Update clustering_coefficients_p2p.py

--- Codes/P2P/clustering_coefficients_p2p.py
+++ Codes/P2P/clustering_coefficients_p2p.py
@@ -20,15 +20,3 @@ for x in sorted_localClustering[:10]:
 AvgClustering = nx.average_clustering(G)
 print("Average Clustering For p2p Network:")
 print(AvgClustering)
-
-#--------------------------------------Global Clustering Coefficient-----------------------------------
-
-Graphtype2 = nx.DiGraph()
-H = nx.from_pandas_edgelist(df, source='Column1', target='Column2', create_using=Graphtype2)
-
-number_of_triangles = sum(nx.triangles(G).values())
-number_of_triads = len(list(nx.all_triads(H)))
-
-GlobalClustering = 3*(number_of_triangles)/number_of_triads
-print("Global Clustering for p2p Network:")
-print(GlobalClustering)
